930f101bd3761cc6ce627821d1b56ede49870203
Anselme Nkondog <nkanven@yahoo.fr>
2016-04-10 19:18:36 +0100

    Creation of front end pages

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,8 +6,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use AppBundle\Entity\Product;
-use AppBundle\Entity\Category;
 
 class DefaultController extends Controller
 {
@@ -17,120 +15,34 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        return $this->render('front-end/index.html.twig');
     }
 
     /**
-     * @Route("lucky/number/{count}", name="lucky-number", defaults={"count" : 1})
+     * @Route("searchit", name="searchit")
      */
-    public function numberAction($count, Request $request)
+    public function searchitAction()
     {
-        if ($count > 0)
-        {
-            $number = array();
+        $request = Request::createFromGlobals();
+        $q = $request->query->get('q');
 
-            for ($i=0; $i < $count ; $i++) 
-            { 
-                $number[] = rand(0, 999);
-            }
-
-            $numberList = implode('-', $number);
-
-            $session = $request->server->get('HTTP_HOST');
-
-            return $this->render('default/number.twig', array('numberList' => $numberList,
-                                                                'session' => $session));
-        }
-
-        throw $this->createNotFoundException($count. " est un nombre nul. Nous n'avaons pas des pages nulles");
-        
+        return $this->render("front-end/searchit.html.twig", array('q'=>$q));
     }
 
     /**
-     * @Route("api/lucky/number", name="api-lucky-number")
+     * @Route("submitit", name="submit")
      */
-    public function apiNumberAction()
+    public function submititAction()
     {
-        $data = array('number' => rand(0, 999));
-
-        return new Response(json_encode($data), 200, array('content-type' => 'application/json'));
+        return $this->render('front-end/searchit.html.twig');
     }
 
     /**
-     * @Route("redirect", name="redirect")
+     * @Route("login", name="login")
      */
-    public function redirectAction()
+    public function logintAction()
     {
-        return $this->redirectToRoute('homepage');
+        return $this->render('front-end/searchit.html.twig');
     }
-
-    /**
-     * @Route("create", name="create")
-     */
-    public function createAction()
-    {
-        $product = new product();
-        $product->setName('Keyboard');
-        $product->setPrice(19.99);
-        $product->setDescription('Ergonomic and stylish');
-
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($product);
-
-        $em->flush();
-
-        return new Response('Saved product with id '.$product->getId());
-    }
-
-    /**
-     * @Route("show/{id}", name="show")
-     */
-    public function showAction($id)
-    {
-        $repository = $this->getDoctrine()
-                            ->getRepository('AppBundle:Product')
-                            ->find($id);
-
-        if (!$repository)
-        {
-            throw $this->createNotFoundException("Le produit avec id ".$id." n'est pas dispo.");
-            
-        }
-
-        return new Response('Produit dispo');
-    }
-
-    /**
-     * @Route("create/product/{id}", name="create_product")
-     */
-    public function createProductAction($id)
-    {
-        $categorie = new category();
-        $categorie->setName('Hardware');
-
-        $repo = $this->getDoctrine()
-                    ->getRepository('AppBundle:Product');
-
-        $product = $repo->find($id);
-        dump($product);
-        if (!$product)
-        {
-            throw $this->createNotFoundException("Le produit avec id ".$id." n'est pas dispo.");
-            
-        }
-        
-        $product->setCategory($categorie);
-
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($categorie);
-        $em->flush();
-
-        return new Response( 'Saved new product with id: '.$product->getId() .' and new category with id: '.$categorie->getId() );
-
-    }
-
-
 
 }
