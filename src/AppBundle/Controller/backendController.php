a349c62b22e8aa4d517832b74fb432284b1edd93
Anselme Nkondog <nkanven@yahoo.fr>
2016-04-15 10:01:52 +0100

    Yml Route setting for backend. Sectorized in bundle, included in app/config. Cpanel set up

---
+++ src/AppBundle/Controller/backendController.php
@@ -0,0 +1,15 @@
+<?php
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class backendController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render("back-end/index.html.twig");
+    }
+}
