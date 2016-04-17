85165c90b4ddc2a35c71450ea3ecfbf8691577dd
Anselme Nkondog <nkanven@yahoo.fr>
2016-04-17 22:10:53 +0100

    Left nav personalized. Blank Pages creation

--- src/AppBundle/Controller/backendController.php
+++ src/AppBundle/Controller/backendController.php
@@ -12,14 +12,81 @@ class backendController extends Controller
     {
         return $this->render("back-end/index.html.twig");
     }
+    /* Lost items controller */
+    public function itemsAction()
+    {
+        return $this->render('back-end/allItems.html.twig');
+    }
+
+    public function addItemAction()
+    {
+        return $this->render('back-end/blank.html.twig');
+    }
+
+    public function itemsCategoriesAction()
+    {
+        return $this->render('back-end/itemsCategories.html.twig');
+    }
+    /* End of lost items controller */
+    /* User item controllers*/
+    public function usersAction($id)
+    {
+        if($id == 0)
+        {
+            return $this->render('back-end/contacts.html.twig');
+        }
+
+        return $this->render('back-end/blank.html.twig');
+    }
+
+    public function addUserAction()
+    {
+        return $this->render('back-end/blank.html.twig');
+    }
+
+    public function usersCategoriesAction()
+    {
+        return $this->render('back-end/usersCategories.html.twig');
+    }
 
     public function profileAction()
     {
         return $this->render('back-end/profile.html.twig');
     }
+    /* End Users controllers*/
 
-    public function usersAction()
+    /* Settings controllers*/
+    public function generalAction()
     {
-        return $this->render('back-end/contacts.html.twig');
+        return $this->render('back-end/general.html.twig');
+    }
+
+    public function financeAction()
+    {
+        return $this->render('back-end/finance.html.twig');
+    }
+
+    public function appearanceAction()
+    {
+        return $this->render('back-end/appearance.html.twig');
+    }
+    /* End of settings controller */
+
+    /* Mailing controllers */
+    public function composeAction()
+    {
+        return $this->render('back-end/blank.html.twig');
+    }
+    public function inboxAction()
+    {
+        return $this->render('back-end/inbox.html.twig');
+    }
+    public function sentAction()
+    {
+        return $this->render('back-end/blank.html.twig');
+    }
+    public function parametersAction()
+    {
+        return $this->render('back-end/blank.html.twig');
     }
 }
